6d5ee4d32ec6883bce7aa0268834cfbc32d4a3b2
salmanahmad2888 <salman.ahmed2888@gmail.com>
2023-07-10 00:40:39 +0500

    Data Transformation Done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from dataclasses import dataclass
 
 import numpy as np 
@@ -10,8 +11,6 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-import os
-
 from src.utils import save_object
 
 @dataclass
---
+++ src/components/model_trainer.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+from src.logger import logging
